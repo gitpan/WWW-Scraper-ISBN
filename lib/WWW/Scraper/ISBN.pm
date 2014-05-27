6847b10e5de154b9020f215897d49b77db89e6cf
Barbie <barbie@missbarbell.co.uk>
2014-05-27 07:28:07 +0000

    Import of BARBIE/WWW-Scraper-ISBN-1.00 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-1.00.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/ISBN.pm
+++ lib/WWW/Scraper/ISBN.pm
@@ -3,7 +3,7 @@ package WWW::Scraper::ISBN;
 use strict;
 use warnings;
 
-our $VERSION = '0.30';
+our $VERSION = '1.00';
 
 #----------------------------------------------------------------------------
 # Library Modules
